1c11b8ff7b4a88bb4fbbabea1147f3994644accd
nguyennguyensy <165183900+nguyennguyensy@users.noreply.github.com>
2025-11-04 16:55:59 +0700

    Delete src directory

--- src/components/landlord/LandlordChat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ChatBox from '../student/ChatBox';
-
-// LandlordChat sử dụng lại component ChatBox của Student vì logic giống nhau
-const LandlordChat = ({ currentUser, draftTargetId, onMessagesRead }) => {
-  return <ChatBox currentUser={currentUser} draftTargetId={draftTargetId} onMessagesRead={onMessagesRead} />;
-};
-
-export default LandlordChat;
